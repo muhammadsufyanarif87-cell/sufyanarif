cc6cbb05ebb6898fba9a03af71afede60d8f404b
muhammadsufyanarif87-cell <muhammadsufyanarif87@gmail.com>
2026-02-17 20:45:30 +0500

    sufuan
    weddite

---
+++ scrip.js
@@ -0,0 +1,14 @@
+function showMessage(){
+    alert("Welcome Sufyan! Your Website is Working 🚀");
+}
+
+function sendForm(){
+    alert("Message Sent Successfully!");
+}
+
+function scrollToSection(id){
+    document.getElementById(id).scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
